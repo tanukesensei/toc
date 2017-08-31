b5250a5042f75d2e89cfd6106a7fe8172ea4a030
Luan Vitola <tanukesensei@users.noreply.github.com>
2017-08-31 10:55:08 -0300

    Login, logout e registro
    Consegui austar os problemas do login, logout e registro, agora posso continuar o desenvolvimento utilizando sessões.

--- app/User.php
+++ app/User.php
@@ -2,16 +2,33 @@
 
 namespace toc;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
-{
+
+class User extends Authenticatable
+{	
     protected $table = 'users';
+    use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = array('name', 'genero', 'datanascimento',
      'cidade', 'estado', 'email', 'username', 'password');
 
     protected $guarded = ['id'];
 
     public $timestamps = false;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
