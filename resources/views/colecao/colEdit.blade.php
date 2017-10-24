876ec6796dbbd2313c22d7c6d960eae581e453c7
Luan Vitola <tanukesensei@users.noreply.github.com>
2017-10-24 21:59:22 -0200

    Alteração na Edição da coleção
    Alterando a blade ColEdit para que fique igual a de cadastro de coleções, ainda falta receber as informações da tabela de editoras, isto está gerando um erro na linha em que lista as editoras.

--- resources/views/colecao/colEdit.blade.php
+++ resources/views/colecao/colEdit.blade.php
@@ -6,16 +6,31 @@
   </head>
   <body>
     <div class="container">
+      
       <form class="" action="/colecao/update/{{$col->id}}" method="post" enctype="multipart/form-data">
         {!! method_field('put') !!}
         <input type="hidden" name="_token" value="{{csrf_token()}}">
+        
         <input type="text" name="nome" value="{{$col->nome}}">
+
         @if ($col->descricao)
         <textarea name="descricao" rows="8" cols="80">{{$col->descricao}}</textarea>
         @else
         <textarea name="descricao" rows="8" cols="80" placeholder="Descrição"></textarea>
         @endif
-        <input type="file" name="foto">
+
+        <input type="text" name="mediapag" value="" placeholder="Número médio de Páginas">
+
+        <input type="text" name="numedicoes" value="" placeholder="Número de Edições">
+
+        <input type="text" name="autor" value="" placeholder="Autor">
+
+        <select class="" name="editora">
+          <option value="" selected disabled hidden>Escolha a editora</option>
+  @foreach ($editora as $e)
+          <option value="{{$e->id}}">{{$e->nome}}</option>
+  @endforeach
+        </select>
 
         <select class="" name="categoria">
           <option value="" selected disabled hidden>Escolha a categoria de sua coleção</option>
@@ -23,6 +38,8 @@
           <option value="{{$c->id}}">{{$c->nome}}</option>
   @endforeach
         </select>
+
+        <input type="file" name="foto">
 
         <input type="hidden" name="usuario" value="{{Auth::user()->id}}">
         <input type="submit" name="enviar" value="Enviar">
