45b2b0c77352e6569e598bfd7777eead9d5e2a2b
Luan Vitola <luanvs@hotmail.com>
2019-02-28 02:21:08 -0300

    Atualização da página do discord
    Página do discord foi levemente modificada para ser melhor  vizualizada no sistema.

--- resources/views/user/userDiscord.blade.php
+++ resources/views/user/userDiscord.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
-<iframe src="https://discordapp.com/widget?id=550311065783894026&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+
+<style media="screen">
+.container2 {
+  margin-top: 75px;
+}
+.row2 {
+  margin-left: 15px;
+  margin-right: 15px;
+}
+</style>
+
+
+<div class="container2">
+
+  <div class="row2">
+
+    <iframe src="https://discordapp.com/widget?id=550311065783894026&theme=dark" width="100%" height="650" allowtransparency="true" frameborder="0"></iframe>
+
+
+  </div>
+
+</div>
 
 @endsection
